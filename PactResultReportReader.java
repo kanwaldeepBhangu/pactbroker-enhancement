d241c223aae615d0bcebbd18655f9e136214c351
kanwaldeepBhangu <er.kanwaldeep.singh@gmail.com>
2018-08-23 11:53:50 +0200

    File which reads tests result from Reports

---
+++ PactResultReportReader.java
@@ -0,0 +1,15 @@
+package enhancement;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PactResultReportReader {
+
+	public String readPactVerifyReportFromTargetDirectry() {
+		String directoryName = System.getProperty("pact.broker.reportFolder");
+        //TODO write logic to make json string to publish
+		return "";
+
+	}
+
+}
