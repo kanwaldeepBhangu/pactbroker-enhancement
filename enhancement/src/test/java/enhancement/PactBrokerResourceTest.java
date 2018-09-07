6020737a4b907aba7499d4dcb55927de7e82bb27
kanwaldeepBhangu <er.kanwaldeep.singh@gmail.com>
2018-09-07 10:26:09 +0200

    code changes

--- enhancement/src/test/java/enhancement/PactBrokerResourceTest.java
+++ enhancement/src/test/java/enhancement/PactBrokerResourceTest.java
@@ -8,44 +8,41 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class PactBrokerResourceTest {
-	
+
 	@BeforeClass
 	public static void setSystemProperties() {
-		System.setProperty("pact.broker.username", "XXX");
-		System.setProperty("pact.broker.password", "XXX");
-		System.setProperty("pact.broker.host", "XXX");
-		System.setProperty("pact.broker.targetFolder", "XXX");
+		System.setProperty("pact.broker.username", "xxx");
+		System.setProperty("pact.broker.password", "xxx");
+		System.setProperty("pact.broker.host", "kanwaldeepsingh.pact.dius.com.au");
+		System.setProperty("pact.broker.targetFolder", "pactFolder");
 	}
-	
-	
+
 	@Test
-	public void testDownloadLatestPactsBasedOnProvider()
-	{
-		PactBrokerResource resource =new PactBrokerResource();
+	public void testDownloadLatestPactsBasedOnProvider() {
+		PactBrokerResource resource = new PactBrokerResource();
 		try {
 			resource.downloadLatestPactsBasedOnProvider("pact_provider");
-		} catch (IOException e) {
+		} catch (PactBrokerUtilityException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			Assert.fail();
 		}
-		
+
 	}
+
 	@Test
-	public void testDownloadLatestPactsBasedOnProviderAndTags()
-	{
-		PactBrokerResource resource =new PactBrokerResource();
+	public void testDownloadLatestPactsBasedOnProviderAndTags() {
+		PactBrokerResource resource = new PactBrokerResource();
 		try {
 			resource.downloadLatestPactsBasedOnProviderAndTags("pact_provider", "dev");
-		} catch (IOException e) {
+		} catch (PactBrokerUtilityException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			Assert.fail();
 		}
 	}
-/*	@Test
-	public void testDownloadLatestPactsBasedOnParticipants()
-	{
-		PactBrokerResource resource =new PactBrokerResource();
-	}*/
+	/*
+	 * @Test public void testDownloadLatestPactsBasedOnParticipants() {
+	 * PactBrokerResource resource =new PactBrokerResource(); }
+	 */
 }
