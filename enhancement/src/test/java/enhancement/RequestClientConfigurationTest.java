db624585ef294262807ce05446abe7b1419ea64d
kanwaldeepBhangu <er.kanwaldeep.singh@gmail.com>
2018-09-12 16:49:21 +0200

    Null check before setting variable

--- enhancement/src/test/java/enhancement/RequestClientConfigurationTest.java
+++ enhancement/src/test/java/enhancement/RequestClientConfigurationTest.java
@@ -5,10 +5,12 @@ import org.junit.Test;
 
 public class RequestClientConfigurationTest {
 
-	@BeforeClass
+		@BeforeClass
 	public static void setSystemProperties() {
-		System.setProperty(PactUtilityConstants.PACT_BROKER_USERNAME, "username");
-		System.setProperty(PactUtilityConstants.PACT_BROKER_PASSWORD, "password");
+		if (System.getProperty(PactUtilityConstants.PACT_BROKER_USERNAME) == null) {
+			System.setProperty(PactUtilityConstants.PACT_BROKER_USERNAME, "username");
+			System.setProperty(PactUtilityConstants.PACT_BROKER_PASSWORD, "password");
+		}
 	}
 
 	@Test
