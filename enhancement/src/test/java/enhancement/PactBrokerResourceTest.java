4ac2a4939eca80ad951abbe1f161c3ad04e847ca
kanwaldeepBhangu <er.kanwaldeep.singh@gmail.com>
2018-09-12 16:40:10 +0200

    env check

--- enhancement/src/test/java/enhancement/PactBrokerResourceTest.java
+++ enhancement/src/test/java/enhancement/PactBrokerResourceTest.java
@@ -21,6 +21,15 @@ public class PactBrokerResourceTest {
 			System.setProperty(PactUtilityConstants.PACT_BROKER_TARGET_FOLDER, "pactFolder");
 		}
 	}
+	
+		@Test
+	public void testEnvVar()
+	{
+		assertEquals("mF97CxbFf5kUBQCWdLk9QMw16UQ8yl", System.getProperty(PactUtilityConstants.PACT_BROKER_USERNAME));
+		assertEquals("82ALVC3qNxc2L0RLhtX34ZJ6lc5Prf", System.getProperty(PactUtilityConstants.PACT_BROKER_PASSWORD));
+		assertEquals("kanwaldeepsingh.pact.dius.com.au", System.getProperty(PactUtilityConstants.PACT_BROKER_HOST));
+		assertEquals("pactFolder", System.getProperty(PactUtilityConstants.PACT_BROKER_TARGET_FOLDER));
+	}
 
 	@Test
 	public void testDownloadLatestPactsBasedOnProvider() {
